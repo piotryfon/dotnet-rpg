83a05a8003a358dfe9a451fc1032dc99955c060b
piotryfon <pit.pit@wp.pl>
2023-03-07 14:11:49 +0100

    Add CharacterService, refactor controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,22 +6,22 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>(){
-            new Character(),
-            new Character{Id = 1, Name = "Sam"}
-        };
-
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+     
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get() => Ok(characters);
+        public ActionResult<List<Character>> Get() => Ok(_characterService.GetAllCharacters());
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id) => Ok(characters.FirstOrDefault(c => c.Id == id));
+        public ActionResult<Character> GetSingle(int id) => Ok(_characterService.GetCharacterById(id));
 
         [HttpPut]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok( _characterService.AddCharacter(newCharacter));
         }
     }
 }
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,10 @@
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    
+    }
+}
