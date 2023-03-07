b40674bd52a63310e19cefad5785bfe018dbc066
piotryfon <pit.pit@wp.pl>
2023-03-07 10:08:26 +0100

    Return Character's list

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,9 +6,14 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>(){
+            new Character(),
+            new Character{Name = "Sam"}
+        };
 
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get() => Ok(characters);
         [HttpGet]
-        public ActionResult<Character> Get() => Ok(knight);
+        public ActionResult<Character> GetSingle() => Ok(characters[0]);
     }
 }
