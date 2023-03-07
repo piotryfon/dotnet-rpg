760718fd1d76925c7413fcf5eaaba26e40f8f1a9
piotryfon <pit.pit@wp.pl>
2023-03-07 09:21:23 +0100

    Dodaj CharacterController

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get() => Ok(knight);
+    }
+}
