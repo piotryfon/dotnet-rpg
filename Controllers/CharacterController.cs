2bedfd67f119c31fb0cef559fc6a09adf3a8ab9d
piotryfon <pit.pit@wp.pl>
2023-03-11 16:14:09 +0100

    Dodaj ServiceResponse

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -13,15 +13,15 @@ namespace dotnet_rpg.Controllers
         }
      
         [HttpGet("GetAll")]
-        public async Task<ActionResult<List<Character>>> Get() => 
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get() => 
             Ok(await _characterService.GetAllCharacters());
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Character>> GetSingle(int id) => 
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id) => 
             Ok(await _characterService.GetCharacterById(id));
 
         [HttpPut]
-        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
         {
             return Ok( await _characterService.AddCharacter(newCharacter));
         }
